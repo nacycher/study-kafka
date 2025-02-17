deeb3ab78172c2559284f483601cfe517d3e877a
nacycher <nacycher@163.com>
2025-02-17 13:44:36 +0800

    git init

---
+++ src/main/java/org/lb/studykafka/StudyKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.lb.studykafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyKafkaApplication.class, args);
+    }
+
+}
